c998626453b7f8b93ac764d18c091b72c5f3025e
Bombatomica_64 <qualcosa2001@gmail.com>
2023-11-15 18:08:34 +0100

    camelv2

--- camel_to_snake.c
+++ camel_to_snake.c
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:13:37 by lmicheli          #+#    #+#             */
-/*   Updated: 2023/11/15 17:27:03 by lmicheli         ###   ########.fr       */
+/*   Updated: 2023/11/15 18:07:49 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,25 +25,39 @@ static int	ft_strlen(char *str)
 
 int	main(int args, char **argv)
 {
-	int	i;
-	int	len;
-	char *str;
+	int		i;
+	int		j;
+	int		len;
+	char	*str;
 
 	i = 0;
 	len = ft_strlen(argv[1]);
 	if (args != 2)
+	{
+		write(1, "\n", 1);
 		return (0);
+	}
+	while (argv[1][i])
+	{
+		if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			len++;
+		i++;
+	}
+	i = 0;
+	j = 0;
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	while(argv[1][i])
 	{
-		if(argv[1][i] == ' ')
-			str[i] = '_';
-		else if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-			str[i] = argv[1][i] + 32;
+		if(argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+		{
+			str[i + j] = '_';
+			j++;
+			str[i + j] = argv[1][i] + 32;
+		}
 		else
-			str[i] = argv[1][i];
+			str[i + j] = argv[1][i];
 		i++;
 	}
-	str[i] = '\0';
+	str[i + j] = '\0';
 	write(1, str, len);
 }
