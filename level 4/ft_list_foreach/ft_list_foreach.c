699bb75fd7f7d9abfd3af0040b6b4c8b3bbd43d4
Bombatomica_64 <qualcosa2001@gmail.com>
2023-11-17 12:17:45 +0100

    rev_wstr

--- level 4/ft_list_foreach/ft_list_foreach.c
+++ level 4/ft_list_foreach/ft_list_foreach.c
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:02:28 by lmicheli          #+#    #+#             */
-/*   Updated: 2023/11/17 10:27:28 by lmicheli         ###   ########.fr       */
+/*   Updated: 2023/11/17 10:39:48 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
 	t_list	*list_ptr;
 
-	
 	list_ptr = begin_list;
 	while (list_ptr)
 	{
